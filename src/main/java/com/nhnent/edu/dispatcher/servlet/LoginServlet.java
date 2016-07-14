e555904f2a4d38a5a569fc7ccaf1e54a9f624f20
신동민/NE <dongmyo@nhnent.com>
2016-07-15 03:49:46 +0900

    * LoginServlet을 Front Controller 도입에 따라 공통 부분 제거하고 viewUrl 전달하도록 수정

--- src/main/java/com/nhnent/edu/dispatcher/servlet/LoginServlet.java
+++ src/main/java/com/nhnent/edu/dispatcher/servlet/LoginServlet.java
@@ -3,7 +3,6 @@ package com.nhnent.edu.dispatcher.servlet;
 import com.nhnent.edu.dispatcher.model.Member;
 import com.nhnent.edu.dispatcher.repository.MemberRepository;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,8 +16,9 @@ public class LoginServlet extends HttpServlet {
     /* 로그인 폼 */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher rd = req.getRequestDispatcher("/login.jsp");
-        rd.forward(req, resp);
+        req.setAttribute("viewUrl", "/login.jsp");
+//        RequestDispatcher rd = req.getRequestDispatcher("/login.jsp");
+//        rd.forward(req, resp);
     }
 
     /* 로그인 처리 */
@@ -33,14 +33,16 @@ public class LoginServlet extends HttpServlet {
 
         Member member = memberRepository.exists(id, password);
         if (member == null) {
-            RequestDispatcher rd = req.getRequestDispatcher("/loginFail.jsp");
-            rd.forward(req, resp);
+            req.setAttribute("viewUrl", "/loginFail.jsp");
+//            RequestDispatcher rd = req.getRequestDispatcher("/loginFail.jsp");
+//            rd.forward(req, resp);
         }
         else {
             HttpSession session = req.getSession();
             session.setAttribute("member", member);
 
-            resp.sendRedirect("/member/list");
+            req.setAttribute("viewUrl", "redirect:/member/list.do");
+//            resp.sendRedirect("/member/list");
         }
     }
 
