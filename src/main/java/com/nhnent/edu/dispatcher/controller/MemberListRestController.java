f5ea5573b8f381b71160978f62ff6def6d01cba8
신동민/NE <dongmyo@nhnent.com>
2017-03-21 03:34:50 +0900

    * @RestController + Jackson2 message converter 적용

---
+++ src/main/java/com/nhnent/edu/dispatcher/controller/MemberListRestController.java
@@ -0,0 +1,25 @@
+package com.nhnent.edu.dispatcher.controller;
+
+import com.nhnent.edu.dispatcher.model.Member;
+import com.nhnent.edu.dispatcher.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+// TODO: 1. /member/list.json RestController 만들어서 실행
+// @RestController = @Controller + @ResponseBody
+@RestController
+public class MemberListRestController {
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @RequestMapping(value = "/member/list.json", method = RequestMethod.GET)
+    public List<Member> getMemberList() {
+        return memberRepository.list();
+    }
+
+}
