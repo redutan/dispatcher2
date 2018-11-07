5c2a0b97234fd0b5a6f306d720ae299b00440e46
조던(myeongju.jung) <myeongju.jung@nhnent.com>
2018-11-07 13:25:46 +0900

    일본어 번역

--- src/main/java/com/nhnent/edu/dispatcher/servlet/MemberListServlet.java
+++ src/main/java/com/nhnent/edu/dispatcher/servlet/MemberListServlet.java
@@ -3,6 +3,7 @@ package com.nhnent.edu.dispatcher.servlet;
 import com.nhnent.edu.dispatcher.model.Member;
 import com.nhnent.edu.dispatcher.repository.MemberRepository;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,12 +21,13 @@ public class MemberListServlet extends HttpServlet {
         List<Member> members = memberRepository.list();
         req.setAttribute("members", members);
 
-        // TODO: Front Controller에서 공통 처리하므로 제거하고 viewUrl 설정
-        req.setAttribute("viewUrl", "/memberList.jsp");
-//        resp.setContentType("text/html; charset=UTF-8");
-//
-//        RequestDispatcher rd = req.getRequestDispatcher("/memberList.jsp");
-//        rd.forward(req, resp);
+        // TODO 5: Front Controller 도입에 따라 공통 부분 제거하고 viewUrl 전달하도록 수정
+        // TODO 5: Front Controller導入により、共通部分を削除してviewUrl伝達するように修正
+//        req.setAttribute
+        resp.setContentType("text/html; charset=UTF-8");
+
+        RequestDispatcher rd = req.getRequestDispatcher("/memberList.jsp");
+        rd.forward(req, resp);
     }
 
 }
